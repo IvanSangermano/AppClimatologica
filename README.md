cc43565e2d5c6b65da205d5c3709ae82d39de691
Ivan Sanger <ivan.j.sanger@gmail.com>
2021-07-10 01:39:22 -0300

    3- Actualizacion de imagenes por medio de los datos obtenidos por la API

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Esta pagina se creo con el fin le lograr informar sobre el clima el la locacion 
 ## Lenguajes utilizados:
 Por medio del "Visual Studio Code" se ha implementado HTML5, CSS y JavaScript para la logica de la pagina Web. Con choques directamente contra api climatologica y de locacion
 ## Commits:
-1- Realizacion de HTML y CSS
+1- Realizacion de HTML y CSS.
+
+2- Fetch contra la api de dia actual.
+
+3- Actualizacion de imagenes por medio de los datos obtenidos por la API
--- app.js
+++ app.js
@@ -1,28 +1,48 @@
 const dias = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const DiaActual = new Date();
+
+var Cancel = document.getElementById("Cancelar");
+Cancel.addEventListener("click", function (){
+    document.getElementById("Busqueda").value = "";
+});
+
 var CuadroDeBusqueda = document.getElementById("Busqueda");
 CuadroDeBusqueda.addEventListener("keypress", function onChangeInput(e){
     if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
         const valueLocation = document.getElementById('Busqueda').value;
-        getLocation(valueLocation)
+        getWeather(valueLocation)
     }
 });
 
+var CuadroDeBusqueda = document.getElementById("Buscar");
+CuadroDeBusqueda.addEventListener("click", function onChangeInput(e){
+        const valueLocation = document.getElementById('Busqueda').value;
+        getWeather(valueLocation)
+});
 
-const getLocation = async (location) => {
+
+const getWeather = async (location) => {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location +"&units=metric&appid=5ff711b4e64edf3a76e5c6c95af460ff");
     const myJson = await response.json();
     console.log(myJson, new Date());
     document.getElementById('Grados').innerHTML = myJson.main.temp + '°C';
-    document.getElementById('Dia').innerHTML = dias[DiaActual.getDay()];
-    document.getElementById('Hora').innerHTML = DiaActual.getHours() + ':' + DiaActual.getMinutes();
+    document.getElementById('Dia').innerHTML = dias[DiaActual.getDay()] + ", ";
+    document.getElementById('Hora').innerHTML =  CorrectlyHoursAndMinute(DiaActual.getHours(), DiaActual.getMinutes());
     document.getElementById('ClimaActualEnString').innerHTML = toUpper(myJson.weather[0].description);
-    try {document.getElementById('PorcentajeDeLluvia').innerHTML = "Rain - " + myJson.rain["1h"] + " mm";} 
-    catch (error) {document.getElementById('PorcentajeDeLluvia').innerHTML = "No Rain";}
+    try {
+        document.getElementById('PorcentajeDeLluvia').innerHTML = "Rain - " + myJson.rain["1h"] + " mm";
+        document.getElementById('PrecipitacionesIcon').src = "Img/Precipitaciones.png";
+    } 
+    catch (error) {
+        document.getElementById('PorcentajeDeLluvia').innerHTML = "No Rain";
+        document.getElementById('PrecipitacionesIcon').src = "Img/NoPrecipitaciones.png";
+    }
     try {
         document.getElementById('CiudadDentroDeImagen').innerHTML = myJson.name.toUpperCase();
     } 
-    catch (error) { document.getElementById('CiudadDentroDeImagen').innerHTML = 'CIUDAD';}
+    catch (error) { 
+        document.getElementById('CiudadDentroDeImagen').innerHTML = 'CIUDAD';
+    }
     document.getElementById('MaxTemperature').innerHTML = 'Max: ' + myJson.main.temp_max + '°C';
     document.getElementById('MinTemperature').innerHTML = 'Max: ' + myJson.main.temp_min + '°C';
     document.getElementById('Humidity').innerHTML = myJson.main.humidity + '%';
@@ -31,6 +51,8 @@ const getLocation = async (location) => {
     document.getElementById('Visibility').innerHTML = myJson.visibility + ' Mts.';
     document.getElementById('Sunrise').innerHTML = SecondToHoursAndMinute(myJson.sys.sunrise) + ' Hs';
     document.getElementById('Sunset').innerHTML = SecondToHoursAndMinute(myJson.sys.sunset)  + ' Hs';
+    SwitchImg(myJson.weather[0].description, "ClimaActualIconoIzq");
+    SwitchImg(myJson.weather[0].description, "TipoDeClimaActualIcono")
 }
 
 function toUpper(str) {
@@ -43,6 +65,16 @@ function toUpper(str) {
         .join(' ');
 }
 
+function CorrectlyHoursAndMinute(hours, minute){
+    if (hours.toString().length == 1) {
+        hours = "0" + hours
+    }
+    if (minute.toString().length == 1) {
+        minute = "0" + minute
+    }
+    return hours + ":" + minute
+}
+
 function SecondToHoursAndMinute(valor){
     // Create a new JavaScript Date object based on the timestamp
     // multiplied by 1000 so that the argument is in milliseconds, not seconds.
@@ -51,10 +83,42 @@ function SecondToHoursAndMinute(valor){
     var hours = date.getHours();
     // Minutes part from the timestamp
     var minutes = "0" + date.getMinutes();
-    // Seconds part from the timestamp
-    var seconds = "0" + date.getSeconds();
     
     // Will display time in 10:30 format
-    return hours + ':' + minutes.substr(-2);
+    return  hours + ':' + minutes.substr(-2);
+}
+
+function SwitchImg(WeatherType, id){
+    switch (WeatherType) {
+        case "clear sky":
+            document.getElementById(id).src = "Img/Soleado.png";
+            break;
+        case "few clouds":
+            document.getElementById(id).src = "Img/ParcialmenteNublado.png";
+            break;
+        case "scattered clouds":
+            document.getElementById(id).src = "Img/Nublado.png";
+            break;
+        case "broken clouds":
+            document.getElementById(id).src = "Img/Churrascos.png";
+            break;
+        case "shower rain":
+            document.getElementById(id).src = "Img/Lluvioso.png";
+            break;
+        case "rain":
+            document.getElementById(id).src = "Img/Lluvioso.png";
+            break;
+        case "thunderstorm":
+            document.getElementById(id).src = "Img/TormentaElectrica.png";
+            break;
+        case "snow":
+            document.getElementById(id).src = "Img/Nevado.png";
+            break;
+        case "mist":
+            document.getElementById(id).src = "Img/Neblina.png";
+            break;
+        default:
+            break;
+    }
 }
 
